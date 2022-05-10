11d8607d9361278bb17b2e2eed320275cbe7d6d2
kurapatimuralikrishna <kuraptimuralikrishna2405@gmail.com>
2022-05-10 19:02:46 +0530

    Added a fix to Borrower.equals()

--- src/main/java/com/murali/ledgerco/model/Borrower.java
+++ src/main/java/com/murali/ledgerco/model/Borrower.java
@@ -27,7 +27,7 @@ public class Borrower {
 		if(obj==null) return false;
 		if(this.getClass()!=obj.getClass()) return false;
 		Borrower borrower = (Borrower) obj;
-		if(this.bankName!=borrower.bankName||this.borrowerName!=borrower.borrowerName) return false;
-		return true;
+		if(this.bankName.equals(borrower.bankName)&&this.borrowerName.equals(borrower.borrowerName)) return true;
+		return false;
 	}
 }
