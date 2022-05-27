5c42636b025bfa236b6d6072fe91334f76512155
kurapatimuralikrishna <kuraptimuralikrishna2405@gmail.com>
2022-05-27 16:55:32 +0530

    rewrote test

--- src/test/java/com/murali/ledgerco/LedgerTest.java
+++ src/test/java/com/murali/ledgerco/LedgerTest.java
@@ -3,6 +3,7 @@ package com.murali.ledgerco;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +11,21 @@ import com.murali.ledgerco.model.Account;
 import com.murali.ledgerco.model.Borrower;
 
 public class LedgerTest {
+	// write test case to see if the account is created
 	@Test
-	public void checkAccounts() {
-		assertEquals(new HashMap<Borrower,Account>(),Ledger.accounts);
+	public void testCreateAccount() {
+		Map<Borrower, Account> testAccounts = new HashMap<>();
+		// test if ledger's account is same as testAccounts
+		assertEquals(Ledger.accounts, testAccounts);
 	}
-	//Test account instantiation
-	//Check getScanner and getCommands methods
-	//when new borrower - test hash code created and equals method
-	//when new account - test loan amount and EMI amount
-	//when payment created, test amount and compare to
-	//when payment added test if present
-	//Check if different methods of account are functioning properly
-	//check Ledger service methods
+	
 }
+
+// Test account instantiation
+// Check getScanner and getCommands methods
+// when new borrower - test hash code created and equals method
+// when new account - test loan amount and EMI amount
+// when payment created, test amount and compare to
+// when payment added test if present
+// Check if different methods of account are functioning properly
+// check Ledger service methods
